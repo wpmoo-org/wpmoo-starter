c4da1c00b72cb38ee9468c86dc092b93096ab4e7
Ahmet Cangir <ahmetcangir07@gmail.com>
2025-10-30 11:59:28 +0100

    chore(starter): migrate to Field::type('id'); phpstan stub for magic statics; config stubFiles

---
+++ phpstan-stubs/wpmoo-fields-field.stub.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * PHPStan stub for WPMoo\Fields\Field magic static constructors.
+ * This file is used only by static analysis; it is not loaded at runtime.
+ */
+
+namespace WPMoo\Fields;
+
+class Field
+{
+    /** @return self */
+    public static function text(string $id) {}
+
+    /** @return self */
+    public static function textarea(string $id) {}
+
+    /** @return self */
+    public static function color(string $id) {}
+
+    /** @return self */
+    public static function checkbox(string $id) {}
+
+    /** @return self */
+    public static function accordion(string $id) {}
+
+    /** @return self */
+    public static function fieldset(string $id) {}
+}
+
