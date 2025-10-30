f660ccb7af9bdecf21fddcd25504858b8269e78c
Ahmet Cangir <ahmetcangir07@gmail.com>
2025-10-30 14:38:38 +0100

    ci(phpstan): add local stub for Field magic static constructors; green analysis

---
+++ phpstan-stubs/wpmoo-fields-field.stub.php
@@ -0,0 +1,19 @@
+<?php
+namespace WPMoo\Fields;
+
+class Field
+{
+    /** @return self */
+    public static function text(string $id) {}
+    /** @return self */
+    public static function textarea(string $id) {}
+    /** @return self */
+    public static function color(string $id) {}
+    /** @return self */
+    public static function checkbox(string $id) {}
+    /** @return self */
+    public static function accordion(string $id) {}
+    /** @return self */
+    public static function fieldset(string $id) {}
+}
+
